8ec1512922b4f1fc7b5d777abbebf79257be7c80
Ranajay Das <ranajay@gmail.com>
2023-11-21 17:24:25 +0800

    Add Prisma DB

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,21 @@
 // This is a server component by default. "use client" must be declared on top to make it a client component
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { redirect } from 'next/navigation'
+import { db } from '@/db'
 
 const Page = async () => {
   const { getUser } = getKindeServerSession()
   const user = await getUser()
 
   if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
+
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  })
+
+  if (!dbUser) redirect('auth-callback?origin=dashboard')
 }
 
 export default Page
